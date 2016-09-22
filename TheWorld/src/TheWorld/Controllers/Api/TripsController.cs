242aacdc7bd4d2591742078412995750410daf50
meelist <Meelis.Talvis@derivco.eu>
2016-09-22 11:30:50 +0300

    Created an api controller with two methods

---
+++ TheWorld/src/TheWorld/Controllers/Api/TripsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TheWorld.Models;
+using TheWorld.ViewModels;
+
+namespace TheWorld.Controllers.Api
+{
+    [Route("api/trips")]
+    public class TripsController : Controller
+    {
+        private readonly IWorldRepository _repo;
+
+        public TripsController(IWorldRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet("")]
+        public IActionResult Get()
+        {
+            //if (true) return BadRequest("Bad things happened");
+
+            return Ok(_repo.GetAllTrips());
+        }
+
+        [HttpPost("")]
+        public IActionResult Post([FromBody]TripViewModel theTrip)
+        {
+            if (ModelState.IsValid)
+            {
+                //Save to the DataBase
+
+                return Created($"api/trips/{theTrip.Name}",true);
+            }
+
+            return BadRequest("Bad data"); //if private api then return BadRequest(ModelState);
+
+        }
+    }
+}
--- TheWorld/src/TheWorld/Startup.cs
+++ TheWorld/src/TheWorld/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
 using TheWorld.Models;
 using TheWorld.Services;
 
@@ -53,7 +54,11 @@ namespace TheWorld
 
          services.AddLogging();
 
-         services.AddMvc();
+         services.AddMvc()
+                .AddJsonOptions(config =>
+             {
+                 config.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+             });
       }
 
       // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
