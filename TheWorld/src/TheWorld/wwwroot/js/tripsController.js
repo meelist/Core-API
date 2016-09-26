7355562f6c03deef62f451a3de64991a27774513
meelist <Meelis.Talvis@derivco.eu>
2016-09-26 18:56:22 +0300

    Commiting work.

---
+++ TheWorld/src/TheWorld/wwwroot/js/tripsController.js
@@ -0,0 +1,26 @@
+﻿// tripsController.js
+
+(function() {
+
+    "use strict";
+
+    // Getting the existing module
+    angular.module("app-trips")
+        .controller("tripsController", tripsController);
+
+    function tripsController() {
+
+        var vm = this;
+
+        vm.trips = [
+        {
+            name: "US Trips",
+            created: new Date()
+        }, {
+            name: "World Trip",
+            createad: new Date()
+        }];
+
+    }
+
+})();
